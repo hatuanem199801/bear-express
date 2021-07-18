c2b70b5743153fa587ba49846488904ae9b7e911
Em Ha Tuan <emhat@nashtecglobal.com>
2021-07-19 01:39:04 +0700

    Add product API

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const express = require("express");
 const app = express();
 
+const product = require("./api/product");
+
 const PORT = 8080 || process.env.PORT;
+
+app.use("/api/product", product);
+
 app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
